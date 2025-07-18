1ff57b280c24a090bb9d28908e985c9c1b5b69d0
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2025-07-18 19:42:36 +0300

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -13,6 +13,7 @@ makedocs(
         "Atoms" => "atoms.md",
         "Systems" => "systems.md",
         "Utilities" => "utilities.md",
+        "Trajectories" => "trajectories.md",
         "Chemfiles Extension" => "chemfiles.md",
         "Index" => "end_index.md",
     ],
